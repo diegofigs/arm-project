a431f88d27a1114b704f2c8d155e232ba795eb6c
Diego Figueroa <diego.fv94@gmail.com>
2016-11-15 22:16:35 -0400

    provide description

--- IncrementerRegister.v
+++ IncrementerRegister.v
@@ -1,8 +1,8 @@
-//Incrementer Register
-
+// Incrementer Register
+// Takes care of saving temporarily the status of the incrementer.
 module IncrementerRegister (output reg [5:0] out, input [5:0] in, input clk);
 
 always @(posedge clk)	
-    out <= in;	
+    out <= in;
 
 endmodule

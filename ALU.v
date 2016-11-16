71e7b13332def30960f29f2ae921cc1a0aae9243
Eduardo Alfonzo <eduardo.alfonzo@upr.edu>
2016-11-16 09:04:58 -0400

    ALU
    + Refactoring
    + Documentation

--- ALU.v
+++ ALU.v
@@ -1,156 +1,127 @@
 /*ALU*/
+module arm_alu(output reg [31:0] result, output reg N, Z, C, V, input [3:0] opcode, input [31:0] A, B, input carry);
+	
+	always @ (A, B, carry, opcode)
+		begin
+			case(opcode)
+				4'b0000: // AND - Logical AND
+					begin
+						result = A & B;
+					end
+				4'b0001: // EOR - Logical EOR
+					begin
+						result = A ^ B;
+						C = carry;
+					end
+				4'b0010: // SUB - Substract
+					begin
+						{C,result} = A - B;
+						C = ~C;
+						update_SUB_V_Flag();
+					end
+				4'b0011: //RSB - Reverse Subtract
+					begin
+						{C,result} = B - A;
+						C = ~C;
+						update_RSB_V_Flag();
+					end
+				4'b0100: // ADD - Addition
+					begin
+						{C,result} = A + B;
+						update_ADD_V_Flag();
+					end
+				4'b0101: // ADC - Addition with Carry
+					begin
+						{C,result} = A + B + C;
+						update_ADD_V_Flag();
+					end
+				4'b0110: // SBC - Subtract with Carry
+					begin
+						C = ~C;
+						{C,result} = A - B - C;
+						C = ~C;
+						update_SUB_V_Flag();
+					end
+				4'b0111: //RSC - Reverse Subtract with Carry
+					begin
+						C = ~C;
+						{C,result} = B - A - C;
+						C = ~C;
+						update_RSB_V_Flag();
+					end
+				4'b1000: //TST - Test (Logical AND, with no results written)
+					begin
+						result = A & B;
+						C = carry;
+					end
+				4'b1001: //TEQ - Test Equivalence (Logical EOR, with no results written)
+					begin
+						result = A ^ B;
+						C = carry;
+					end
+				4'b1010: //CMP - Compare (SUB, with no results written)
+					begin
+						{C,result} = A - B;
+						C = ~C;
+						update_SUB_V_Flag();
+					end
+				4'b1011: //CMN - Compare Negated (NOT ADD, with no results written)
+					begin
+						{C,result} = A + B;
+						update_ADD_V_Flag();
+					end
+				4'b1100: // ORR - Logical OR
+					begin
+						result = A | B;
+						C = carry;
+					end
+				4'b1101: //MOV - Move
+					begin
+						result = B;
+						C = carry;
+					end
+				4'b1110: //BIC - Bit Clear
+					begin
+						result = A & ~B;
+						C = carry;
+					end
+				4'b1111: //MVN - Move Not
+					begin
+						result = ~B;
+						C = carry;
+					end
+			endcase
 
-module arm_alu (output reg [31:0] result, output reg N_flag, Z_flag, C_flag, V_flag, input [3:0] op_code, input [31:0] dataA, dataB, input carry);
-	reg [31:0] temp;
-	always @ (*)
-	case (op_code)
-		4'b0000: //[AND] Logical AND
-			begin
-				result = dataA & dataB;
-				C_flag = carry;
-				update_N_Z_flags();
-			end
-		4'b0001: //[EOR] Logical Exclusive OR
-			begin
-				result = (dataA & ~dataB) | (~dataA & dataB);
-				C_flag = carry;
-				update_N_Z_flags();
-			end
-		4'b0010: //[SUB] Subtract
-			begin
-				{C_flag, result} = dataA - dataB;
-				if((dataA[31] == 0 && dataB[31] == 1 && result[31] == 1) || (dataA[31] == 1 && dataB[31] == 0 && result[31] == 0))
-					V_flag = 1;
-				else
-					V_flag = 0;
-				update_N_Z_flags();
-			end
-		4'b0011: //[RSB] Reverse Subtract
-			begin
-				{C_flag, result} = dataB - dataA;
-				if((dataB[31] == 0 && dataA[31] == 1 && result[31] == 1) || (dataB[31] == 1 && dataA[31] == 0 && result[31] == 0))
-					V_flag = 1;
-				else
-					V_flag = 0;
-				update_N_Z_flags();
-			end
-		4'b0100: //[ADD] Add
-			begin
-				{C_flag, result} = dataA + dataB;
-				if((dataA[31] == 0 && dataB[31] == 0 && result[31] == 1) || (dataA[31] == 1 && dataB[31] == 1 && result[31] == 0))
-					V_flag = 1;
-				else
-					V_flag = 0;
-				update_N_Z_flags();
-			end
-		4'b0101: //[ADC] Add with Carry
-			begin
-				{C_flag, result} = dataA + dataB + carry;
-				if((dataA[31] == 0 && dataB[31] == 0 && result[31] == 1) || (dataA[31] == 1 && dataB[31] == 1 && result[31] == 0))
-					V_flag = 1;
-				else
-					V_flag = 0;
-				update_N_Z_flags();
-			end
-		4'b0110: //[SBC] Subtract with Carry
-			begin
-				{C_flag, result} = dataA - dataB + carry;
-				if((dataA[31] == 0 && dataB[31] == 1 && result[31] == 1) || (dataA[31] == 1 && dataB[31] == 0 && result[31] == 0))
-					V_flag = 1;
-				else
-					V_flag = 0;
-				update_N_Z_flags();
-			end
-		4'b0111: //[RSC] Reverse Subtract with Carry
-			begin
-				{C_flag, result} = dataB - dataA + carry;
-				if((dataB[31] == 0 && dataA[31] == 1 && result[31] == 1) || (dataB[31] == 1 && dataA[31] == 0 && result[31] == 0))
-					V_flag = 1;
-				else
-					V_flag = 0;
-				update_N_Z_flags();
-			end
-		4'b1000: //[TST] Test
-			begin
-				temp = dataA & dataB;
-				C_flag = carry;
-				N_flag = temp[31];
-					if(temp == 0) 
-						Z_flag = 1;
-					else 
-						Z_flag = 0;
-			end
-		4'b1001: //[TEQ] Test Equivalence
-			begin
-				temp = (dataA & ~dataB) | (~dataA & dataB);
-				C_flag = carry;
-				N_flag = temp[31];
-					if(temp == 0) 
-						Z_flag = 1;
-					else 
-						Z_flag = 0;
-			end
-		4'b1010: //[CMP] Compare
-			begin
-				{C_flag, temp} = dataA - dataB;
-				N_flag = temp[31];
-					if(temp == 0) 
-						Z_flag = 1;
-					else 
-						Z_flag = 0;
-			end
-		4'b1011: //[CMN] Compare Negated
-			begin
-				{C_flag, temp} = dataA + dataB;
-				N_flag = temp[31];
-					if(temp == 0) 
-						Z_flag = 1;
-					else 
-						Z_flag = 0;
-			end
-		4'b1100: //[ORR] Logical OR
-			begin
-				result = dataA | dataB;
-				C_flag = carry;
-				update_N_Z_flags();
-			end
-		4'b1101: //[MOV] Move
-			begin
-				result = dataB;
-				C_flag = carry;
-				update_N_Z_flags();
-			end
-		4'b1110: //[BIC] Bit Clear
-			begin
-				result = dataA & ~dataB;
-				C_flag = carry;
-				update_N_Z_flags();
-			end
-		4'b1111: //[MVN] Move Not
-			begin	
-				result = ~dataB;
-				C_flag = carry;
-				update_N_Z_flags();
-			end
-		/*5'b10000: //Add 4 PC
-			begin
-				result = dataB + 4;
-			end*/
-	endcase
+			N = result[31]; // Set Negative (N) Flag to the result's most significant bit
+			if(result == 0) Z = 1; // Set Zero (Z) Flag
+			else Z = 0;
+		end
 
-	task update_N_Z_flags; //Update the N & Z flags
-	begin
-		N_flag = result[31];
-			if(result == 0) 
-				Z_flag = 1;
+		task update_ADD_V_Flag; // Check for Overflow when adding
+		begin
+			if((A[31] == 0 && B[31] == 0 && result[31] == 1) || (A[31] == 1 && B[31] == 1 && result[31] == 0))
+				V=1;
 			else 
-				Z_flag = 0;
-	end
-	endtask
-
+				V=0;
+		end
+		endtask
+		
+		task update_SUB_V_Flag; // Check for overflow when substracting
+		begin
+			if((A[31] == 0 && B[31] == 1 && result[31] == 1) || (A[31] == 1 && B[31] == 0 && result[31] == 0))
+				V=1;
+			else 
+				V=0;
+		end
+		endtask
+		
+		task update_RSB_V_Flag; // Check for overflow when reverse substracting
+		begin
+			if((B[31] == 0 && A[31] == 1 && result[31] == 1) || (B[31] == 1 && A[31] == 0 && result[31] == 0))
+				V=1;
+			else 
+				V=0;
+		end
+		endtask
+		
 endmodule
-
-
-
-
-

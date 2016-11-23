adf6905783ed9a91e6c1532e714b903197874ab2
Eduardo Alfonzo <eduardo.alfonzo@upr.edu>
2016-11-23 19:59:07 -0400

    Immediate Sign Extension
    ! basic cleanup & refactoring

--- ImmediateSignExtension.v
+++ ImmediateSignExtension.v
@@ -1,57 +1,57 @@
 // Sign Extension
 
-module Imme_Sign_Extension (output reg [31:0] Y, input [31:0] In, input signEN, input [1:0] imme_SEL);
+module Imme_Sign_Extension (output reg [31:0] Y, input [31:0] in, input enable, input [1:0] immeSEL);
 
 always @ (*)
 
 begin
-	if (!signEN)
+	if (!enable)
 	begin
-		case (imme_SEL)
+		case (immeSEL)
 			2'b00:
 			begin
-				Y[7:0] = In[7:0];
-				Y[31:8] = {24{In[7]}};
+				Y[7:0] = in[7:0];
+				Y[31:8] = {24{in[7]}};
 			end
 			2'b01:
 			begin
-				Y[11:0] = In[11:0];
-				Y[31:12] = {20{In[7]}};
+				Y[11:0] = in[11:0];
+				Y[31:12] = {20{in[7]}};
 			end
 			2'b10:
 			begin
-				Y[23:0] = In[23:0];
-				Y[31:24] = {8{In[7]}};
+				Y[23:0] = in[23:0];
+				Y[31:24] = {8{in[7]}};
 			end
 			2'b11:
 			begin
-				Y[7:0] = {In[11:8], In[3:0]};
-				Y[31:8] = {24{In[7]}};
+				Y[7:0] = {in[11:8], in[3:0]};
+				Y[31:8] = {24{in[7]}};
 			end
 		endcase
 	end
 	
 	else
 	begin
-		case (imme_SEL)
+		case (immeSEL)
 			2'b00:
 			begin
-				Y[7:0] = In[7:0];
+				Y[7:0] = in[7:0];
 				Y[31:8] = 24'b000000000000000000000000;
 			end
 			2'b01:
 			begin
-				Y[11:0] = In[11:0];
+				Y[11:0] = in[11:0];
 				Y[31:12] = 20'b00000000000000000000;
 			end
 			2'b10:
 			begin
-				Y[23:0] = In[23:0];
+				Y[23:0] = in[23:0];
 				Y[31:24] = 8'b00000000;
 			end
 			2'b11:
 			begin
-				Y[7:0] = {In[11:8], In[3:0]};
+				Y[7:0] = {in[11:8], in[3:0]};
 				Y[31:8] = 24'b000000000000000000000000;
 			end
 		endcase

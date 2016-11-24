627b32efdc9ba5b08753c76e370dbf737ed68522
Eduardo Alfonzo <eduardo.alfonzo@upr.edu>
2016-11-23 20:00:53 -0400

    Pipeline Register
    ! basic cleanup

--- PipelineRegister.v
+++ PipelineRegister.v
@@ -1,10 +1,6 @@
-//--------------------------------------------------------
-//				PIPELINE REGISTER
-//--------------------------------------------------------
-
+// PIPELINE REGISTER
 module PipelineRegister (output reg [54:0] out, input [54:0] pipeline_in, input clk);
 
 always @(posedge clk)	
     out <= pipeline_in;	
-
 endmodule

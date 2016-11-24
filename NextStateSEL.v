89fd56bb18643d21b25709de0efe148936ef7f2c
Eduardo Alfonzo <eduardo.alfonzo@upr.edu>
2016-11-23 20:00:37 -0400

    Next State Selector
    ! basic cleanup & refactoring

--- NextStateSEL.v
+++ NextStateSEL.v
@@ -1,95 +1,91 @@
-//---------------------------------------------------------------
-//                  NEXT STATE ADDRESS SELECTOR
-//---------------------------------------------------------------
-
-module Nxt_St_Sel (output reg M0, output reg M1, input cond_S, input [2:0] pipeline);
+// NEXT STATE ADDRESS SELECTOR
+module Nxt_St_Sel (output reg m0, output reg m1, input condS, input [2:0] pipeline);
 
 initial begin
-	M0 = 1'b1;
-	M1 = 1'b0;
+	m0 = 1'b1;
+	m1 = 1'b0;
 end
-
 
 always @ (*)
 case (pipeline)
 
-	3'b000: //Encoder
+	3'b000: // Encoder
 	begin
-		M0 = 1'b0;
-		M1 = 1'b0;
+		m0 = 1'b0;
+		m1 = 1'b0;
 	end
 	
-	3'b001: //Fetch
+	3'b001: // Fetch
 	begin
-		M0 = 1'b1;
-		M1 = 1'b0;
+		m0 = 1'b1;
+		m1 = 1'b0;
 	end
 	
-	3'b010: //Pipeline
+	3'b010: // Pipeline
 	begin
-		M0 = 1'b0;
-		M1 = 1'b1;
+		m0 = 1'b0;
+		m1 = 1'b1;
 	end
 	
-	3'b011: //Incrementer 
+	3'b011: // Incrementer 
 	begin
-		M0 = 1'b1;
-		M1 = 1'b1;
+		m0 = 1'b1;
+		m1 = 1'b1;
 	end
 	
 	3'b100:
 	begin
-		if(cond_S == 1'b1)
+		if(condS == 1'b1)
 		begin
-			M0 = 1'b0;
-			M1 = 1'b1;
+			m0 = 1'b0;
+			m1 = 1'b1;
 		end
 		else
 		begin
-			M0 = 1'b0;
-			M1 = 1'b0;
+			m0 = 1'b0;
+			m1 = 1'b0;
 		end 
 	end	
 	
 	3'b101:
 	begin
-		if(cond_S == 1'b1)
+		if(condS == 1'b1)
 		begin
-			M0 = 1'b0;
-			M1 = 1'b1;
+			m0 = 1'b0;
+			m1 = 1'b1;
 		end
 		else
 		begin
-			M0 = 1'b1;
-			M1 = 1'b1;
+			m0 = 1'b1;
+			m1 = 1'b1;
 		end
 	end
 	
 	3'b110:
 	begin
-		if(cond_S == 1'b1)
+		if(condS == 1'b1)
 		begin
-			M0 = 1'b0;
-			M1 = 1'b0;
+			m0 = 1'b0;
+			m1 = 1'b0;
 		end
 		else
 		begin
-			M0 = 1'b1;
-			M1 = 1'b1;
+			m0 = 1'b1;
+			m1 = 1'b1;
 		end
 	end
 	
 	3'b111:
 	begin
-		if(cond_S == 1'b1)
+		if(condS == 1'b1)
 		begin
-			M0 = 1'b0;
-			M1 = 1'b1;
+			m0 = 1'b0;
+			m1 = 1'b1;
 		end
 		else
 		begin
-			M0 = 1'b1;
-			M1 = 1'b0;
+			m0 = 1'b1;
+			m1 = 1'b0;
 		end
 	end
 endcase

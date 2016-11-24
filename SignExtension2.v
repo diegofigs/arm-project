61e7f3c714ce7500c98f6ad07f801738a16d5d08
Eduardo Alfonzo <eduardo.alfonzo@upr.edu>
2016-11-23 20:01:21 -0400

    Sign Extension 2
    ! basic cleanup & refactoring

--- SignExtension2.v
+++ SignExtension2.v
@@ -1,56 +1,33 @@
 // Sign Extension for Memory Data
 
-module Sign_Extension2 (output reg [31:0] Y, input [31:0] In, input [1:0] Data_Type, input enable);
-
+module Sign_Extension2 (output reg [31:0] Y, input [31:0] in, input [1:0] dataType, input enable);
    
-   always @ (In, Data_Type)
+always @ (in, dataType)
 
-      if (!enable)
-
-         begin
-
-            case(Data_Type)
-
-      		    2'b00: //Byte
-
-      			   begin 
-
-      				  assign Y[7:0] = In[7:0];
-      				  assign Y[31:8] = {24{In[7]}};
-
-      			   end
-
-         		2'b01: //Halfword
-
-         			begin
-
-         				assign Y[15:0] = In[15:0];
-         				assign Y[31:16] = {16{In[15]}};
-
-         			end
-
-         		2'b10: //Word
-
-         			begin
-
-         				assign Y = In;
-
-         			end
-
-
-         		2'b11: //Doubleword
-
-         			begin
-
-         				assign Y = In;
-
-         			end
-            endcase
-
-         end
-
-      else
-
-         assign Y = In;
+begin
+   if (!enable)
+      case(dataType)
+		    2'b00: // Byte
+			   begin 
+				  assign Y[7:0] = in[7:0];
+				  assign Y[31:8] = {24{in[7]}};
+			   end
+   		2'b01: // Halfword
+   			begin
+   				assign Y[15:0] = in[15:0];
+   				assign Y[31:16] = {16{in[15]}};
+   			end
+   		2'b10: // Word
+   			begin
+   				assign Y = in;
+   			end
+   		2'b11: // Doubleword
+   			begin
+   				assign Y = in;
+   			end
+      endcase
+   else
+      assign Y = in;
+end
 
 endmodule
